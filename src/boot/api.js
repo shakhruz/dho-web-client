90725445e5cd80ac92048d9c5cbd076b55025d74
Sebastian <smontero@mightywizards.com>
2021-08-09 18:57:33 -0500

    Removed hyperion dependencies

--- src/boot/api.js
+++ src/boot/api.js
@@ -100,7 +100,7 @@ const getBestEndpoint = async () => {
 const pingEndpoint = async (url) => {
   const start = Date.now()
   try {
-    await axios.get(`${url}/v2/health`, {
+    await axios.post(`${url}/v1/get_info`, {
       timeout: 5000
     })
   } catch (e) {
